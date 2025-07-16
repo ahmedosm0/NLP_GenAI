97bc9614e5c0ea0960103974f67e87a3b85071b8
ahmedosm0 <ahmadusman050@gmail.com>
2025-07-17 01:28:06 +0500

    updated

--- langchain_projects/10.text_sumerization_link/app.py
+++ langchain_projects/10.text_sumerization_link/app.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
-
+    
 
 
 ## sstreamlit APP
